8a31c47f7ceb6e5992e4875a44f2a1b74617b498
Delos Chang <lol.i.laugh@gmail.com>
2013-02-16 20:09:24 -0500

    creating new binary tree

---
+++ BinaryTreeHuffman.java
@@ -0,0 +1,17 @@
+package PS4;
+
+
+public class BinaryTreeHuffman extends BinaryTree{
+	private char dataCharacter;
+	private int dataFrequency;
+
+	/* 
+	 *  Constructs singleton tree
+	 *  storing two values: character and frequency
+	 */
+	public BinaryTreeHuffman(Object data) {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+}
